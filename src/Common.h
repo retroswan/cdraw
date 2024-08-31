41add8a62ba9b3292ba1a5a401d3c482aa7b0ad1
retroswan <world5ziggurat@gmail.com>
2024-08-30 21:53:32 -0400

    Ported TheSpyDog's example over!!!!!

---
+++ src/Common.h
@@ -0,0 +1,94 @@
+#ifndef SDL_GPU_EXAMPLES_H
+#define SDL_GPU_EXAMPLES_H
+
+#include <SDL3/SDL.h>
+
+typedef struct Context
+{
+	const char* ExampleName;
+	const char* BasePath;
+	SDL_Window* Window;
+	SDL_GPUDevice* Device;
+	SDL_bool LeftPressed;
+	SDL_bool RightPressed;
+	SDL_bool DownPressed;
+	SDL_bool UpPressed;
+	float DeltaTime;
+} Context;
+
+int CommonInit(Context* context, SDL_WindowFlags windowFlags);
+void CommonQuit(Context* context);
+
+void InitializeAssetLoader();
+SDL_Surface* LoadImage(const char* imageFilename, int desiredChannels);
+float* LoadHDRImage(const char* imageFilename, int* pWidth, int* pHeight, int* pChannels, int desiredChannels);
+
+SDL_GPUShader* LoadShader(
+	SDL_GPUDevice* device,
+	const char* shaderFilename,
+	Uint32 samplerCount,
+	Uint32 uniformBufferCount,
+	Uint32 storageBufferCount,
+	Uint32 storageTextureCount
+);
+SDL_GPUComputePipeline* CreateComputePipelineFromShader(
+	SDL_GPUDevice* device,
+	const char* shaderFilename,
+	SDL_GPUComputePipelineCreateInfo* createInfo
+);
+
+// Vertex Formats
+typedef struct PositionVertex
+{
+	float x, y, z;
+} PositionVertex;
+
+typedef struct PositionColorVertex
+{
+	float x, y, z;
+	Uint8 r, g, b, a;
+} PositionColorVertex;
+
+typedef struct PositionTextureVertex
+{
+    float x, y, z;
+    float u, v;
+} PositionTextureVertex;
+
+// Matrix Math
+typedef struct Matrix4x4
+{
+	float m11, m12, m13, m14;
+	float m21, m22, m23, m24;
+	float m31, m32, m33, m34;
+	float m41, m42, m43, m44;
+} Matrix4x4;
+
+typedef struct Vector3
+{
+	float x, y, z;
+} Vector3;
+
+Matrix4x4 Matrix4x4_Multiply(Matrix4x4 matrix1, Matrix4x4 matrix2);
+Matrix4x4 Matrix4x4_CreateRotationZ(float radians);
+Matrix4x4 Matrix4x4_CreateTranslation(float x, float y, float z);
+Matrix4x4 Matrix4x4_CreateOrthographicOffCenter(float left, float right, float bottom, float top, float zNearPlane, float zFarPlane);
+Matrix4x4 Matrix4x4_CreatePerspectiveFieldOfView(float fieldOfView, float aspectRatio, float nearPlaneDistance, float farPlaneDistance);
+Matrix4x4 Matrix4x4_CreateLookAt(Vector3 cameraPosition, Vector3 cameraTarget, Vector3 cameraUpVector);
+Vector3 Vector3_Normalize(Vector3 vec);
+float Vector3_Dot(Vector3 vecA, Vector3 vecB);
+Vector3 Vector3_Cross(Vector3 vecA, Vector3 vecB);
+
+// Examples
+typedef struct Example
+{
+	const char* Name;
+	int (*Init)(Context* context);
+	int (*Update)(Context* context);
+	int (*Draw)(Context* context);
+	void (*Quit)(Context* context);
+} Example;
+
+extern Example ComputeSpriteBatch_Example;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,246 @@
+#include "Common.h"
+#include <SDL3/SDL_main.h>
+
+static Example* Examples[] =
+{
+	&ComputeSpriteBatch_Example,
+};
+
+SDL_bool AppLifecycleWatcher(void *userdata, SDL_Event *event)
+{
+	/* This callback may be on a different thread, so let's
+	 * push these events as USER events so they appear
+	 * in the main thread's event loop.
+	 *
+	 * That allows us to cancel drawing before/after we finish
+	 * drawing a frame, rather than mid-draw (which can crash!).
+	 */
+	if (event->type == SDL_EVENT_DID_ENTER_BACKGROUND)
+	{
+		SDL_Event evt;
+		evt.type = SDL_EVENT_USER;
+		evt.user.code = 0;
+		SDL_PushEvent(&evt);
+	}
+	else if (event->type == SDL_EVENT_WILL_ENTER_FOREGROUND)
+	{
+		SDL_Event evt;
+		evt.type = SDL_EVENT_USER;
+		evt.user.code = 1;
+		SDL_PushEvent(&evt);
+	}
+	return SDL_FALSE;
+}
+
+int main(int argc, char **argv)
+{
+	Context context = { 0 };
+	int exampleIndex = -1;
+	int gotoExampleIndex = 0;
+	int quit = 0;
+	float lastTime = 0;
+
+	for (int i = 1; i < argc; i += 1)
+	{
+		if (SDL_strcmp(argv[i], "-name") == 0 && argc > i + 1)
+		{
+			const char* exampleName = argv[i + 1];
+			int foundExample = 0;
+
+			for (int j = 0; j < SDL_arraysize(Examples); j += 1)
+			{
+				if (SDL_strcmp(Examples[j]->Name, exampleName) == 0)
+				{
+					gotoExampleIndex = j;
+					foundExample = 1;
+					break;
+				}
+			}
+
+			if (!foundExample)
+			{
+				SDL_Log("No example named '%s' exists.", exampleName);
+				return 1;
+			}
+		}
+	}
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD) < 0)
+	{
+		SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+		return 1;
+	}
+
+	InitializeAssetLoader();
+	SDL_AddEventWatch(AppLifecycleWatcher, NULL);
+
+	SDL_Log("Welcome to the SDL_GPU example suite!");
+	SDL_Log("Press A/D (or LB/RB) to move between examples!");
+
+	SDL_Gamepad* gamepad = NULL;
+	SDL_bool canDraw = SDL_TRUE;
+
+	while (!quit)
+	{
+		context.LeftPressed = 0;
+		context.RightPressed = 0;
+		context.DownPressed = 0;
+		context.UpPressed = 0;
+
+		SDL_Event evt;
+		while (SDL_PollEvent(&evt))
+		{
+			if (evt.type == SDL_EVENT_QUIT)
+			{
+				if (exampleIndex != -1)
+				{
+					Examples[exampleIndex]->Quit(&context);
+				}
+				quit = 1;
+			}
+			else if (evt.type == SDL_EVENT_GAMEPAD_ADDED)
+			{
+				if (gamepad == NULL)
+				{
+					gamepad = SDL_OpenGamepad(evt.gdevice.which);
+				}
+			}
+			else if (evt.type == SDL_EVENT_GAMEPAD_REMOVED)
+			{
+				if (evt.gdevice.which == SDL_GetGamepadID(gamepad))
+				{
+					SDL_CloseGamepad(gamepad);
+				}
+			}
+			else if (evt.type == SDL_EVENT_USER)
+			{
+				if (evt.user.code == 0)
+				{
+#ifdef SDL_PLATFORM_GDK
+					SDL_GDKSuspendGPU(context.Device);
+					canDraw = SDL_FALSE;
+					SDL_GDKSuspendComplete();
+#endif
+				}
+				else if (evt.user.code == 1)
+				{
+#ifdef SDL_PLATFORM_GDK
+					SDL_GDKResumeGPU(context.Device);
+					canDraw = SDL_TRUE;
+#endif
+				}
+			}
+			else if (evt.type == SDL_EVENT_KEY_DOWN)
+			{
+				if (evt.key.key == SDLK_D)
+				{
+					gotoExampleIndex = exampleIndex + 1;
+					if (gotoExampleIndex >= SDL_arraysize(Examples)) {
+						gotoExampleIndex = 0;
+					}
+				}
+				else if (evt.key.key == SDLK_A)
+				{
+					gotoExampleIndex = exampleIndex - 1;
+					if (gotoExampleIndex < 0) {
+						gotoExampleIndex = SDL_arraysize(Examples) - 1;
+					}
+				}
+				else if (evt.key.key == SDLK_LEFT)
+				{
+					context.LeftPressed = SDL_TRUE;
+				}
+				else if (evt.key.key == SDLK_RIGHT)
+				{
+					context.RightPressed = SDL_TRUE;
+				}
+				else if (evt.key.key == SDLK_DOWN)
+				{
+					context.DownPressed = SDL_TRUE;
+				}
+				else if (evt.key.key == SDLK_UP)
+				{
+					context.UpPressed = SDL_TRUE;
+				}
+			}
+			else if (evt.type == SDL_EVENT_GAMEPAD_BUTTON_DOWN)
+			{
+				if (evt.gbutton.button == SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER)
+				{
+					gotoExampleIndex = exampleIndex + 1;
+					if (gotoExampleIndex >= SDL_arraysize(Examples)) {
+						gotoExampleIndex = 0;
+					}
+				}
+				else if (evt.gbutton.button == SDL_GAMEPAD_BUTTON_LEFT_SHOULDER)
+				{
+					gotoExampleIndex = exampleIndex - 1;
+					if (gotoExampleIndex < 0) {
+						gotoExampleIndex = SDL_arraysize(Examples) - 1;
+					}
+				}
+				else if (evt.gbutton.button == SDL_GAMEPAD_BUTTON_DPAD_LEFT)
+				{
+					context.LeftPressed = SDL_TRUE;
+				}
+				else if (evt.gbutton.button == SDL_GAMEPAD_BUTTON_DPAD_RIGHT)
+				{
+					context.RightPressed = SDL_TRUE;
+				}
+				else if (evt.gbutton.button == SDL_GAMEPAD_BUTTON_DPAD_DOWN)
+				{
+					context.DownPressed = SDL_TRUE;
+				}
+				else if (evt.gbutton.button == SDL_GAMEPAD_BUTTON_DPAD_UP)
+				{
+					context.UpPressed = SDL_TRUE;
+				}
+			}
+		}
+		if (quit)
+		{
+			break;
+		}
+
+		if (gotoExampleIndex != -1)
+		{
+			if (exampleIndex != -1)
+			{
+				Examples[exampleIndex]->Quit(&context);
+				SDL_zero(context);
+			}
+
+			exampleIndex = gotoExampleIndex;
+			context.ExampleName = Examples[exampleIndex]->Name;
+			SDL_Log("STARTING EXAMPLE: %s", context.ExampleName);
+			if (Examples[exampleIndex]->Init(&context) < 0)
+			{
+				SDL_Log("Init failed!");
+				return 1;
+			}
+
+			gotoExampleIndex = -1;
+		}
+
+		float newTime = SDL_GetTicks() / 1000.0f;
+		context.DeltaTime = newTime - lastTime;
+		lastTime = newTime;
+
+		if (Examples[exampleIndex]->Update(&context) < 0)
+		{
+			SDL_Log("Update failed!");
+			return 1;
+		}
+
+		if (canDraw)
+		{
+			if (Examples[exampleIndex]->Draw(&context) < 0)
+			{
+				SDL_Log("Draw failed!");
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+}
